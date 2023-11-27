c0ba3e47f2eb1d465a27fd0b7a403c3b690759c3
Jhenbert Villamucho <jvv205756@gmail.com>
2023-11-27 22:53:30 +0800

    added content

--- front_end/src/pages/HomePage.tsx
+++ front_end/src/pages/HomePage.tsx
@@ -1,5 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className="mt-5">
+      <h1 className="font-bold text-2xl">Welcome to Web-based POS</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem
+        placeat esse repellat earum recusandae ad, maiores nulla fugiat
+        exercitationem autem, reprehenderit magnam ratione! Repudiandae officia
+        voluptates, et provident at sunt!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum facilis
+        recusandae, aut consectetur dolorem reprehenderit natus cum repellendus
+        dicta? Sunt beatae laboriosam ratione quia omnis aut id voluptatibus non
+        tenetur.
+      </p>
+      <Button asChild className="mt-4">
+        <Link to="pos">Click to Sell your Products</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default HomePage;
--- front_end/src/pages/POSPage.tsx
+++ front_end/src/pages/POSPage.tsx
@@ -1,5 +1,55 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { useFetch } from "@jhenbertnpm/use-fetch";
+
+type Products = {
+  id: number;
+  product_name: string;
+  price: number;
+  img: string;
+};
+
 const POSPage = () => {
-  return <div>POS Page</div>;
+  const { data } = useFetch<Products[]>({
+    fetchFn: async () => {
+      const response = await fetch("http://localhost:3000/products");
+      const result = await response.json();
+      return result;
+    },
+    initData: [],
+  });
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-4">
+      {data.map((product) => (
+        <Card key={product.id}>
+          <CardHeader>
+            <h1 className="font-bold text-2xl">{product.product_name}</h1>
+          </CardHeader>
+          <CardContent>
+            <div className="aspect-w-16 aspect-h-9 relative bg-foreground/5 dark:bg-background rounded-lg">
+              <img
+                src={product.img}
+                alt={product.product_name}
+                className="w-full h-full object-center object-cover rounded-lg transition-all duration-300 hover:scale-105"
+                style={{ cursor: "pointer" }}
+              />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <div>
+              <p className="text-sm font-medium">{product.product_name}</p>
+              <p className="text-sm">Php{product.price.toFixed(2)}</p>
+            </div>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
 };
 
 export default POSPage;

23eb4e367b45919389342645f51408bfcf6f76d2
Jhenbert Villamucho <jvv205756@gmail.com>
2023-12-04 23:54:46 +0800

    added hero image

--- front_end/src/pages/HomePage.tsx
+++ front_end/src/pages/HomePage.tsx
@@ -3,23 +3,35 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div className="mt-5">
-      <h1 className="font-bold text-2xl">Welcome to Web-based POS</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem
-        placeat esse repellat earum recusandae ad, maiores nulla fugiat
-        exercitationem autem, reprehenderit magnam ratione! Repudiandae officia
-        voluptates, et provident at sunt!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum facilis
-        recusandae, aut consectetur dolorem reprehenderit natus cum repellendus
-        dicta? Sunt beatae laboriosam ratione quia omnis aut id voluptatibus non
-        tenetur.
-      </p>
-      <Button asChild className="mt-4">
-        <Link to="pos">Click to Sell your Products</Link>
-      </Button>
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 p-5 mt-5">
+      <div>
+        <h1 className="font-bold text-3xl mb-4">Welcome to Web-based POS</h1>
+        <p className="text-lg text-justify">
+          Revolutionize your business with our cutting-edge web-based point of
+          sale (POS) solution. Say goodbye to traditional cash registers and
+          hello to a seamless and efficient sales experience. Our marketable
+          web-based POS empowers you to manage transactions, inventory, and
+          customer interactions from anywhere with an internet connection. Enjoy
+          the flexibility of real-time data access, allowing you to make
+          informed decisions on-the-go. With user-friendly interfaces and
+          customizable features, our web-based POS is designed to enhance your
+          sales process, streamline operations, and boost overall productivity.
+          Elevate your business to new heights with a modern and marketable POS
+          solution that adapts to your needs and helps you stay ahead in today's
+          dynamic market.
+        </p>
+
+        <Button asChild className="mt-4">
+          <Link to="pos">Click to Sell your Products</Link>
+        </Button>
+      </div>
+      <div>
+        <img
+          src="https://images.pexels.com/photos/12935074/pexels-photo-12935074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="hero-img"
+          className="rounded-md"
+        />
+      </div>
     </div>
   );
 };
